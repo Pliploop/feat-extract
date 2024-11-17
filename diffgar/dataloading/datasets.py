8fa5a21460fdd3afdd05e80b6c2290750b82e4ec
Pliploop <jngt@hotmail.fr>
2024-11-16 19:24:36 -0800

    error extraction removal

--- diffgar/dataloading/datasets.py
+++ diffgar/dataloading/datasets.py
@@ -188,9 +188,8 @@ class TextAudioDataset(Dataset):
                 
                 yield audio_features, file_path
             except Exception as e:
-                print(f"Error extracting features: {e}")
-                print(e)
-        
+                pass
+            
     def extract_and_save_features(self, model, save_dir = None, extract_method = 'extract_features', extract_kwargs = {}, out_key = 'embedding', hop = None, return_full_audio = True, limit_n = None, save = False, verbose = True, root_path = None):
         
         
